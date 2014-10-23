3e9ad8732bf700ab411cc85115ed0e81413ab0b6
Logan King <logan@rocketjones.com>
2014-10-23 14:43:34 -0600

    Initial commit of site content

---
+++ html/journal.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>online journal</title>
+</head>
+<body>
+
+	<article>
+		<h1>beginnings</h1>
+		<p>This is the first post. I'm planning on writing more. 
+It's important for me to make a beginning somewhere. So instead of 
+overthinking it, as I'm aught to do, this is it. Terse. Simple. The 
+next piece will be about something else. Not a second beginning.</p>
+	</article>
+
+</body>
+
+</html>
